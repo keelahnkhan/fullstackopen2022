394174918d265c3f7efe56edcaba93c78a238d46
kpt9408@rit.edu <sirmasterdufel@gmail.com>
2023-01-06 11:35:21 -0500

    Exercise 2.8, added ability to add number along with name to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,27 +2,38 @@ import React, { useState } from 'react';
 
 const App = () => {;
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
+    { name: 'Arto Hellas', number: '040-123-4567'}
   ]);
-  const [newName, setNewName] = useState('');
+  const [newPerson, setNewPerson] = useState({name: '', number: ''});
 
   const updatePersons = (event) => {
     event.preventDefault();
 
-    if (-1 !== persons.findIndex((person) => person.name === newName)) {
-      alert(`${newName} is already added to the phonebook`);
+    if (-1 !== persons.findIndex((person) => person.name === newPerson.name)) {
+      alert(`${newPerson.name} is already added to the phonebook`);
       return;
     }
 
-    const newPerson = { name: newName };
+    if (!(newPerson.name.trim() && newPerson.number.trim())) {
+      alert(`Both name and number are required`);
+      return;
+    }
+
     const newPersons = persons.concat(newPerson);
     setPersons(newPersons);
-    setNewName('');
+    setNewPerson({name: '', number: ''});
   }
 
   const changeName = (event) => {
     console.log("name changed", event.target.value);
-    setNewName(event.target.value);
+    const person = { ...newPerson, name: event.target.value};
+    setNewPerson(person);
+  } 
+
+  const changeNumber = (event) => {
+    console.log("number changed", event.target.value);
+    const person = { ...newPerson, number: event.target.value};
+    setNewPerson(person);
   }
 
   return (  
@@ -30,7 +41,10 @@ const App = () => {;
       <h2>PhoneBook</h2>
       <form onSubmit={updatePersons}>
         <div>
-          name: <input value={newName} onChange={changeName}/>
+          name: <input value={newPerson.name} onChange={changeName}/>
+        </div>
+        <div>
+          number: <input value={newPerson.number} onChange={changeNumber}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -38,7 +52,10 @@ const App = () => {;
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => <p key={person.name}>{person.name}</p>)}
+        {persons.map((person) => 
+          <p key={person.name}>
+            {person.name} {person.number}
+          </p>)}
       </div>
     </div>
   );
