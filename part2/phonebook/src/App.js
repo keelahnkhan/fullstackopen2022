16cae591710e688e752c877db7128791dd7acdcd
kpt9408@rit.edu <sirmasterdufel@gmail.com>
2023-01-06 10:34:41 -0500

    Exercise 2.6, implemented rudimentary phonebook with ability to add new names

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const App = () => {;
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas'}
+  ]);
+  const [newName, setNewName] = useState('');
+
+  const updatePersons = (event) => {
+    event.preventDefault();
+    const newPerson = { name: newName };
+    const newPersons = persons.concat(newPerson);
+    setPersons(newPersons);
+    setNewName('');
+  }
+
+  const changeName = (event) => {
+    console.log("name changed", event.target.value);
+    setNewName(event.target.value);
+  }
+
+  return (  
+    <div>
+      <h2>PhoneBook</h2>
+      <form onSubmit={updatePersons}>
+        <div>
+          name: <input value={newName} onChange={changeName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => <p key={person.name}>{person.name}</p>)}
+      </div>
+    </div>
+  );
+}
+ 
+export default App;
