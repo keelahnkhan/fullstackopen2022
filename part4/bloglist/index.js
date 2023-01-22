7437901cea231902bc3d644123db61f2b36f8755
Keelan Tucker <kpt9408@rit.edu>
2023-01-21 22:18:38 -0500

    Exercise 4.2, refactor modle

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -4,15 +4,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog');
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 mongoose.connect(process.env.MONGO_URI)
 
