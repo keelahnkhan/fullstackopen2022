4f383f0beda4e998b59a8cf29d97f8c32ab9f653
Keelan Tucker <kpt9408@rit.edu>
2023-01-21 23:54:02 -0500

    Exercise 4.5, added favoriteBlog function with tests

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -1,5 +1,46 @@
-const {dummy} = require('../utils/list_helper');
+const listHelper = require('../utils/list_helper');
+const {blogs, blogsSingle} = require('./data');
 
 test('dummy returns 1', () => {
-  expect(dummy([])).toBe(1);
+  expect(listHelper.dummy([])).toBe(1);
 });
+
+describe('total likes', () => {
+
+  test('when one blog, likes should equal the one blog', () => {
+    expect(listHelper.totalLikes(blogsSingle)).toBe(7);
+  });
+
+  test('when multiple blogs, likes should equal the sum of each blog', () => {
+    expect(listHelper.totalLikes(blogs)).toBe(36);
+  });
+
+  test('when no blogs given, likes should equal 0', () => {
+    expect(listHelper.totalLikes([])).toBe(0);
+  });
+});
+
+describe('favorite blog', () => {
+  
+  test('when no blogs, favorite blog should be empty', () => {
+    expect(listHelper.favoriteBlog([])).toEqual({});
+  });
+
+  test('when one blog, favorite blog should be the one blog', () => {
+    expect(listHelper.favoriteBlog(blogsSingle)).toEqual(blogsSingle[0]);
+  });
+
+  test('when multiple logs, favorite blog should be blog with most likes', () => {
+    expect(listHelper.favoriteBlog(blogs)).toEqual(
+      {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+      }
+    );
+  });
+});
+
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,25 @@
 const dummy = (blogs) => {
   return 1;
-}
+};
+
+const totalLikes = (blogs) => {
+  const accumulator = (sum, blog) => {
+    return sum + blog.likes;
+  }
+
+  return blogs.reduce(accumulator, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  const favorite = (prevBig, blog) => {
+      return prevBig.likes > blog.likes ? prevBig : blog;
+  }
+
+  return blogs.length == 0 ? {} : blogs.reduce(favorite);
+};
 
 module.exports = {
-  dummy
+  dummy, 
+  totalLikes, 
+  favoriteBlog
 };
